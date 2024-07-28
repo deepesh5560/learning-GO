4f72551c361a93e27e91b1acd81a67c40d95e391
deepesh5560 <deepesh.sharma@chroextech.com>
2024-07-28 12:06:43 +0530

    maps

---
+++ maps/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myMap = make(map[string]string)
+	myMap["py"] = "python"
+	myMap["js"] = "javascript"
+	myMap["rb"] = "ruby"
+	myMap["rs"] = "Rust"
+
+	fmt.Println("myMaps", myMap)
+	fmt.Println("here is Js", myMap["js"])
+
+	// maps with loops
+
+	for key, value := range myMap {
+		fmt.Println("here is key value", key, value)
+
+	}
+}
