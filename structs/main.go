10ff020f2e41168debda4a78011f60a4b0cd4282
deepesh5560 <dssharma912@gmail.com>
2024-07-28 12:29:43 +0530

    structs

--- structs/main.go
+++ structs/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+	// almost same as TS object interfaces and structs in rust
 	deep := User{"deepesh", "deepesh@go.dev", 24, true}
 	fmt.Println("User as deep", deep)
 	fmt.Println("User name and mail", deep.Name, deep.Mail)
