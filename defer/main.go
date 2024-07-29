f2fe59e6cacb0d1888f19ad715e38c8d38f146f4
deepesh5560 <dssharma912@gmail.com>
2024-07-29 12:06:21 +0530

    defer

---
+++ defer/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// defer is a keyword when we use it it pushes that piece of code in the end of function
+	// if there are multiple defer in the function it will be executed in reverse order (last in first out)
+	defer fmt.Println("hello")
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+	fmt.Println("World")
+	mydefer()
+} //result world two one hello
+
+func mydefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
