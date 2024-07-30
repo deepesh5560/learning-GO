0768f906b23b55e99b57c11998f229194558be84
deepesh5560 <dssharma912@gmail.com>
2024-07-30 21:57:17 +0530

    get,post Api

--- hadling_api_request/main.go
+++ hadling_api_request/main.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "https://api.restful-api.dev/objects"
+const postUrl = "https://dummyjson.com/posts/add"
 
 func main() {
 	fmt.Println("")
+	getApi()
+	postApi()
+}
+
+func getApi() {
 
 	response, err := http.Get(url)
 
@@ -28,6 +35,18 @@ func main() {
 	result := string(dataBytes)
 
 	fmt.Println("Result", result)
+
+	defer response.Body.Close()
+}
+
+func postApi() {
+	requestBody := strings.NewReader(`{
+	"title":"I am in love with someone.",
+	"userId":5
+	}`) // creating JSON
+	response, _ := http.Post(postUrl, "application/json", requestBody)
+	result, _ := ioutil.ReadAll(response.Body)
+	println("post", string(result))
 
 	defer response.Body.Close()
 }
